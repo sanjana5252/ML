fdc3789efab2d8d76592e1b6ffb08abca3394cd0
sanjana5252 <140432909+sanjana5252@users.noreply.github.com>
2023-09-10 22:09:06 +0530

    Add files via upload

---
+++ binomial logistic regression.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+#Logistic Regression binomial
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+# load the breast cancer dataset
+X, y = load_breast_cancer(return_X_y=True)
+# split the train and test dataset
+X_train, X_test,\
+    y_train, y_test = train_test_split(X, y,
+                                       test_size=0.20,
+                                       random_state=23)
+# LogisticRegression
+clf = LogisticRegression(random_state=0)
+clf.fit(X_train, y_train)
+# Prediction
+y_pred = clf.predict(X_test)
+ 
+acc = accuracy_score(y_test, y_pred)
+print("Logistic Regression model accuracy (in %):", acc*100)
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+
+
+
+# In[16]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
